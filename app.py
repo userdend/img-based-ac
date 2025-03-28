db6ce269d4bbf7bb82607fe67a9b859a3d90d04c
userdend <boydbayndend99@gmail.com>
2025-03-28 11:54:54 +0800

    Database; Preference; Menu; Highlighting; Images

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import tkinter as tk
-from tkinter import ttk, filedialog
+from tkinter import ttk, filedialog, simpledialog, messagebox
 from PIL import Image, ImageTk
+import os
+import sqlite3
+
+# Setup database.
+conn = sqlite3.connect("app.db")
+cursor  =conn.cursor()
+cursor.execute("CREATE TABLE IF NOT EXISTS preference (id INTEGER PRIMARY KEY, name TEXT)")
+cursor.execute("CREATE TABLE IF NOT EXISTS sequence_order (id INTEGER PRIMARY KEY, preference_id INTEGER, name TEXT, FOREIGN KEY (preference_id) REFERENCES preference(id))")
+conn.commit()
 
 # Create main window.
 root = tk.Tk()
@@ -13,10 +22,112 @@ root.resizable(False, False)
 # Menu bar.
 menu_bar = tk.Menu(root)
 
+# Dictionary to store file path.
+file_dict = {}
+
+def import_image():
+    file_path = filedialog.askopenfilenames(filetypes = [("Image Files", "*.*")])
+
+    for file in file_path:
+        # Get the file name.
+        file_name = os.path.basename(file)
+
+        # If file already exists.
+        existing_item = sequence_order.get(0, tk.END)
+
+        if file_name in existing_item:
+            index = existing_item.index(file_name)
+            sequence_order.delete(index)
+
+        # Store the file path in dictionary.
+        file_dict[file_name] = file
+
+        # Insert the new file.
+        sequence_order.insert(tk.END, file_name)
+
+def add_preference():
+    # Check if preference exists.
+    def preference_exists(pref):
+        preference_item = preference.get(0, tk.END)
+        return pref in preference_item
+
+    new_preference = simpledialog.askstring("New Preference", "Enter preference name:")
+
+    if new_preference:
+        new_preference = new_preference.strip()
+
+        if new_preference:
+            if preference_exists(new_preference):
+                messagebox.showwarning("Warning", "Preference name already exists.")
+                return
+
+            # Add preference to the database.
+            cursor.execute("INSERT INTO preference (name) VALUES (?)", (new_preference,))
+            conn.commit()
+
+            # Insert the item into Listbox.
+            preference.insert(tk.END, new_preference)
+        else:
+            messagebox.showwarning("Warning", "Preference name cannot be empty!")
+
+def rename_preference():
+    selected_index = preference.curselection()
+
+    if selected_index:
+        index = selected_index[0]
+        current_preference_name = preference.get(index)
+
+        # Ask user for new name.
+        new_preference_name = simpledialog.askstring("Rename Preference", prompt = "Enter new name:", initialvalue = current_preference_name)
+
+        if new_preference_name:
+            new_preference_name = new_preference_name.strip()
+
+            if new_preference_name:
+                # Update into database.
+                cursor.execute("UPDATE preference SET name = ? WHERE name = ?", (new_preference_name, current_preference_name))
+                conn.commit()
+
+                # Update Listbox.
+                preference.delete(index)
+                preference.insert(index, new_preference_name)
+            else:
+                messagebox.showwarning("Warning", "Preference name cannot be empty!")
+
+def delete_preference():
+    selected_index = preference.curselection()
+    
+    if selected_index:
+        # Get the preference name.
+        preference_name = preference.get(selected_index)
+
+        # Update database.
+        cursor.execute("DELETE FROM preference WHERE name = ?", (preference_name,))
+        conn.commit()
+
+        # Delete from Listbox.
+        preference.delete(selected_index[0])
+
+def show_preference_context_menu(event):
+    selected_index = preference.nearest(event.y)
+    preference.selection_clear(0, tk.END)
+    preference.selection_set(selected_index)
+    preference.activate(selected_index)
+
+    context_menu.post(event.x_root, event.y_root)
+    root.after(100, lambda: root.bind("<Button-1>", lambda e: context_menu.unpost()))
+
+def display_selected_preference(event):
+    selected_index = preference.curselection()
+
+    if selected_index:
+        index = preference.get(selected_index[0])
+        preference_selected_indicator.config(text = f"Selected Preference: {index}")
+
 # File menu.
 file_menu = tk.Menu(menu_bar, tearoff = 0)
-file_menu.add_command(label = "New Preference", command = root.quit)
-file_menu.add_command(label = "Import Images", command = root.quit)
+file_menu.add_command(label = "New Preference", command = add_preference)
+file_menu.add_command(label = "Import Images", command = import_image)
 file_menu.add_separator()
 file_menu.add_command(label = "Exit", command = root.quit)
 
@@ -56,6 +167,8 @@ class DragDropListBox(tk.Listbox):
 
             # Update new index.
             self._drag_data["index"] = index
+            self.selection_clear(0, tk.END)
+            self.selection_set(index)
 
     def on_release(self, event):
         # Clear the drag data upon dropping.
@@ -73,34 +186,41 @@ preference_label = tk.Label(preference_frame, text = "Preferences", font = ("Ari
 preference_label.pack()
 
 preference_scrollbar = tk.Scrollbar(preference_frame, orient = "vertical")
-preference = tk.Listbox(preference_frame, height = 10, width = 20, yscrollcommand = preference_scrollbar.set)
+preference = tk.Listbox(preference_frame, height = 10, width = 20, yscrollcommand = preference_scrollbar.set, selectmode=tk.SINGLE, exportselection=False)
 preference_scrollbar.config(command = preference.yview)
 
 preference.pack(side = "left")
 preference_scrollbar.pack(side = "left", fill = "y")
 
-# Frame for priority order.
-priority_order_frame = tk.Frame(frame)
-priority_order_frame.pack(side = "left", padx = 10)
+# Context menu for preference upon right clicking.
+context_menu = tk.Menu(root, tearoff = 0)
+context_menu.add_command(label = "Rename", command = rename_preference)
+context_menu.add_command(label = "Delete", command = delete_preference)
 
-priority_order_label = tk.Label(priority_order_frame, text = "Priority Order", font = ("Aria", 10, "bold"))
-priority_order_label.pack()
+preference.bind("<Button-3>", show_preference_context_menu)
+preference.bind("<<ListboxSelect>>", display_selected_preference)
 
-priority_order_scrollbar = tk.Scrollbar(priority_order_frame, orient = "vertical")
-priority_order = DragDropListBox(priority_order_frame, height = 10, width = 20, yscrollcommand = priority_order_scrollbar.set)
-priority_order_scrollbar.config(command = priority_order.yview)
+# Frame for sequence order.
+sequence_order_frame = tk.Frame(frame)
+sequence_order_frame.pack(side = "left", padx = 10)
 
-priority_order.pack(side = "left")
-priority_order_scrollbar.pack(side = "left", fill = "y")
+sequence_order_label = tk.Label(sequence_order_frame, text = "Sequence Order", font = ("Aria", 10, "bold"))
+sequence_order_label.pack()
+
+sequence_order_scrollbar = tk.Scrollbar(sequence_order_frame, orient = "vertical")
+sequence_order = DragDropListBox(sequence_order_frame, height = 10, width = 20, yscrollcommand = sequence_order_scrollbar.set, selectmode=tk.SINGLE, exportselection=False)
+sequence_order_scrollbar.config(command = sequence_order.yview)
+
+sequence_order.pack(side = "left")
+sequence_order_scrollbar.pack(side = "left", fill = "y")
 
 # Separator object.
 separator = ttk.Separator(root, orient = "horizontal")
 separator.pack(fill = "x")
 
-# Add items
-for i in range(1, 21):
-    preference.insert(tk.END, f"Item {i}")
-    priority_order.insert(tk.END, f"Value {i}")
+# Text widget to show selected preference.
+preference_selected_indicator = tk.Label(root, text = "Selected Preference: ")
+preference_selected_indicator.pack(pady = 5)
 
 # Root.
 root.mainloop()
